e09bb3015c92c205dbd0eb620f706d31f16b373c
root <root@localhost>
2022-09-16 04:54:47 +0000

    best

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(title: 'harry potter', author: 'jk rowling', price: 100, pubDate: '1999-1-1')
+  end
+end
